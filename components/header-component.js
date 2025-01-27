618724306a144d7ee5e1de76a950bc7fec35c91f
juancarlosfc5 <181221774+juancarlosfc5@users.noreply.github.com>
2025-01-27 18:41:41 -0500

    feat: :bug: v7 - Bug Bootstrap
    Fixed bug in import of Bootstrap

--- components/header-component.js
+++ components/header-component.js
@@ -10,7 +10,7 @@ export class HeaderComponent extends LitElement {
 
   render() {
     return html`
-      <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
       <div class="container text-center card">
         <div class="row align-items-start card-header">
           <h3>Apple Store - No. Factura</h3>
