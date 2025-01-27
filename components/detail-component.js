fb74f342bb989867b1d93cb2e811ef350a761af7
juancarlosfc5 <181221774+juancarlosfc5@users.noreply.github.com>
2025-01-27 18:50:18 -0500

    fix: :bug: v9 - Change Bootstrap import to links

--- components/detail-component.js
+++ components/detail-component.js
@@ -22,7 +22,7 @@ export class DetailComponent extends LitElement {
 
   render() {
     return html`
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
       <br />
       <div class="container card">
         <div class="row text-center card-header">
